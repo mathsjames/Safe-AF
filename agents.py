b2f77ec7b8184d15bac1846e0b18f99a3f879aba
LindaLinsefors <linda.linsefors@gmail.com>
2018-04-14 18:05:03 +0200

    edited cooling function

--- agents.py
+++ agents.py
@@ -26,31 +26,11 @@ class More_Advanced_Softmax:
         return e_x / e_x.sum(axis=0)
 
 
-class More_Advanced_Softma_2x:
-
-<<<<<<< HEAD
-
-=======
->>>>>>> d471620b3f91c3cc3ecdbea7f7264f38ac41b8bf
-    def __init__(self, cooling_function):
-        self.cooling_function = cooling_function
-        #Cooling function should be a function from number of games played to temperature
-
-    def function(self, x, games_played):
-        temperature = self.cooling_function(games_played)
-        x = [i/temperature for i in x]
-        e_x = np.exp(x-np.min(x))
-        return e_x / e_x.sum(axis=0)
-
-<<<<<<< HEAD
-
-=======
->>>>>>> d471620b3f91c3cc3ecdbea7f7264f38ac41b8bf
 def exponential_cooling(games_played):
     # Example of cooling function
-    temperature = 1000*(0.999**games_played)
+    temperature = 100*(0.99**games_played)
     return temperature
-<<<<<<< HEAD
+
 
 =======
 >>>>>>> d471620b3f91c3cc3ecdbea7f7264f38ac41b8bf
