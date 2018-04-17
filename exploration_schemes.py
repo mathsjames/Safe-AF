12151329f6d85919580decaba6cb098c2e530071
unknown <joar.mvs@gmail.com>
2018-04-17 13:12:25 +0100

    unfinished changed

--- exploration_schemes.py
+++ exploration_schemes.py
@@ -15,6 +15,9 @@ class Epsilon_Greedy:
             mxs = list(filter(lambda x: x == mx, x))
             return [1.0/len(mxs) if i==mx else 0 for i in x]
 
+# Epsilon-First?
+# Epsilon Decreasing?
+
 class Softmax:
 
     def __init__(self, temperature):
@@ -37,20 +40,33 @@ class More_Advanced_Softmax:
         e_x = np.exp(x-np.max(x))
         return e_x / e_x.sum(axis=0)
 
-#def exponential_cooling(games_played):
-#    # Example of cooling function
-#    if  100*(0.99**games_played) > 0.00001:
-#        temperature = 100*(0.99**games_played)
-#    else:
-#        temperature = 0.00001
-#    return temperature
-
 class exponential_cooling:
     def function(self, games_played):
-        # Example of cooling function
-        if  100*(0.99**games_played) > 0.00001:
+        if  100*(0.99**games_played) > 0.000001:
             temperature = 100*(0.99**games_played)
         else:
-            temperature = 0.00001
-        #print(temperature)
+            temperature = 0.000001
         return temperature
+
+
+class UCB_exploration_scheme:
+
+    def __init__(self, c):
+        self.c = c
+
+    def function(self, exp, std):
+
+        return 0
+
+    def choose(self, agent):
+        exploration = np.log(agent.t+1) / agent.action_attempts
+        exploration[np.isnan(exploration)] = 0
+        exploration = np.power(exploration, 1/self.c)
+
+        q = agent.value_estimates + exploration
+        action = np.argmax(q)
+        check = np.where(q == action)[0]
+        if len(check) == 0:
+            return action
+        else:
+            return np.random.choice(check)
